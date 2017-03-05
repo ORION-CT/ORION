80cf446bec91cfcab143483e8a6d6db0a0b38c0c
vikram.bhandari@amphorainc.com <Vikram123>
2017-03-05 12:09:51 -0500

    Video

--- video/app.py
+++ video/app.py
@@ -2,10 +2,10 @@
 from flask import Flask, render_template, Response
 
 # emulated camera
-from camera import Camera
+# from camera import Camera
 
 # Raspberry Pi camera module (requires picamera package)
-# from camera_pi import Camera
+from camera_pi import Camera
 
 app = Flask(__name__)
 
